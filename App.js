4864b1ecdff0dea74094d666d84881f29ac65a46
Zach <zachlandis91@gmail.com>
2024-02-16 13:34:32 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, } from 'react-native';
 
-export default function App() {
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
